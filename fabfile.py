6e735021e95b060a026b73b706aac20f038eb7d1
syx <783284481@qq.com>
2017-06-28 16:20:19 +0800

    使用 Fabric 自动化部署

---
+++ fabfile.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+from fabric.api import env, run
+from fabric.operations import sudo
+
+GIT_REPO = 'git@github.com:shanyongxu/blog.git'
+
+env.user = 'root'
+
+env.password = '!@#5841314521lkh'
+
+env.hosts = ['www.syx666.com']
+
+env.port = '22'
+
+def deploy():
+    
+    source_folder = '/home/shanyx/sites/syx666.com/blog'
+    run('cd %s && git pull' % source_folder)
+    run("""
+        cd {} &&
+        ../env/bin/pip install -r requirements.txt &&
+        ../env/bin/python3.4 manage.py collectstatic --noinput &&
+        ../env/bin/python3.4 manage.py makemigrations &&
+        ../env/bin/python3.4 manage.py migrate
+        """.format(source_folder)) 
+    run('restart gunicorn-www.syx666.com')
+    run('service nginx restart')
+
