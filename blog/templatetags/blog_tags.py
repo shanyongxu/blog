f5dbac93213900113aea9d98350272d3e86eda6a
syx <783284481@qq.com>
2017-06-29 17:28:55 +0800

    标签云

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -18,6 +18,8 @@ def archives():
 def get_categories():
     return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
 
-
+@register.simple_tag
+def get_tags():
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
 
 
--- blog/views.py
+++ blog/views.py
@@ -95,3 +95,13 @@ def category(request, pk):
     cate = get_object_or_404(Category, pk=pk);
     post_list = Post.objects.filter(category=cate).order_by('-create_time')
     return render(request, 'blog/index.html', context={'post_list':post_list})
+
+
+class TagView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'post_list'
+    
+    def get_queryset(self):
+        tag = get_object_or_404(Tag, pk = self.kwargs.get('pk'))
+        return super(TagView, self).get_queryset().filter(tags=tag)
