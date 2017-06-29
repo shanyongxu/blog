899b6f501986b08513ce1b15fcf29fd2b379a1b8
syx <783284481@qq.com>
2017-06-29 18:03:06 +0800

    RSS 订阅

---
+++ blog/feeds.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+from django.contrib.syndication.views import Feed
+from blog.models import *
+
+class AllPostsRssFeed(Feed):
+    title = "Django 博客教程演示项目"
+    link = "/"
+    description = "Django 博客教程演示项目测试文章"
+    # 需要显示的内容条目
+    def items(self):
+        return Post.objects.all()
+
+    # 聚合器中显示的内容条目的标题
+    def item_title(self, item):
+        return '[%s] %s' % (item.category, item.title)
+
+    # 聚合器中显示的内容条目的描述
+    def item_description(self, item):
+        return item.body
--- blogproject/urls.py
+++ blogproject/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-
+from blog.feeds import *
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
     url(r'',include('comments.urls')),
+    url(r'^all/rss/$', AllPostsRssFeed(), name='rss'),
 ]
